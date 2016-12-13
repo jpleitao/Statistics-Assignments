87be0bac707b830cc89dbd613f2ae00d1a66d74c
jpleitao <jocaleitao93@gmail.com>
2016-12-13 21:46:22 +0000

    Code to draw histograms overlapped with probability density for the Normal, Gamma, Beta, Logistic, Cauchy and Student distributions

--- A2/code/DrawHistogram.R
+++ A2/code/DrawHistogram.R
@@ -4,7 +4,7 @@
 
 # Determines if only the frequency histogram of the data is plotted, or if a
 # density of a probability distribution is also plotted
-PLOT_DENSITY = FALSE
+PLOT_DENSITY = 'Logistic'
 
 DrawHistogram <- function(dataset) {
   #
@@ -12,16 +12,84 @@ DrawHistogram <- function(dataset) {
   #   dataset: An array of doubles with the data whose histogram will be plotted.
   #
   #
-  if (PLOT_DENSITY) {
-    hist(dataset, freq=FALSE)
+  
+  meanDataset = mean(dataset)
+  sdDataset = sd(dataset)
+  
+  if (PLOT_DENSITY == 'Normal') {
+    # Plot the histogram and add the corresponding distribution
+    histTitle = paste('Histogram with Normal density (u=', meanDataset,
+                       ', sigma=', sdDataset)
     
-    # The following line is just here because the distribution resembles the
-    # Normal distribution and will only be added to overlap the normal curve to
-    # the obtained histogram
-    curve(dnorm(x, mean=mean(dataset), sd=sd(dataset)), col="darkblue", lwd=2,
+    hist(dataset, freq=FALSE, main=histTitle)
+    
+    # Overlap the normal curve to the obtained histogram
+    curve(dnorm(x, mean=meanDataset, sd=sdDataset), col="darkblue", lwd=2,
           add=TRUE)
-  } else {
+    
+  } else if (PLOT_DENSITY == 'Gamma') {
+    # Values for the parameters obtained by solving a system of equations
+    distShape = meanDataset^2/sdDataset^2
+    distScale = sdDataset^2/meanDataset
+    
+    # Plot the histogram and add the corresponding distribution
+    histTitle = paste('Histogram with Gamma density (k=', distShape,
+                       ', teta=', distScale)
+      
+    hist(dataset, freq=FALSE, main=histTitle)
+    
+    # Overlap the gamma curve to the obtained histogram
+    curve(dgamma(x, shape=distShape, scale=distScale), col="darkblue", lwd=2,
+          add=TRUE)
+    
+  } else if (PLOT_DENSITY == 'Beta') {
+    # Values for the parameters obtained by solving a system of equations, using
+    # Wolfram Alpha. Please visit:
+    # <url>https://www.wolframalpha.com/input/?i=x%2F(x%2By)%3Da,+(x+*+y)%2F(+
+    # (x+%2B+y+%2B+1)+*+(x+%2B+y)+*+(x+%2B+y)+)%3Db%5E2</url>
+    distShape1 = (-(meanDataset^3) + (meanDataset^2) -
+                     meanDataset*(sdDataset^2)) / (sdDataset^2)
+    
+    distShape2 = ( (meanDataset)^3 -2*(meanDataset^2) +
+                      meanDataset*(sdDataset^2) + meanDataset -
+                      (sdDataset^2) ) / (sdDataset^2)
+    
+    # Plot the histogram and add the corresponding distribution
+    histTitle = paste('Histogram with Beta density (alfa=', distShape1,
+                       ', beta=', distShape2)
+    
+    hist(dataset, freq=FALSE, main=histTitle)
+    
+    # Overlap the beta curve to the obtained histogram
+    curve(dbeta(x, shape1=distShape1, shape2=distShape2), col="darkblue",
+          lwd=2, add=TRUE)
+    
+    # The call to the function dbeta with these parameters fires a warning
+    # stating that NaN values were produced. Clearly this is not an adequate
+    # distribution to model the given dataset!
+    
+  } else if (PLOT_DENSITY == 'Logistic') {
+    # Values for the parameters obtained by solving a system of equations
+    distLoc = meanDataset
+    distScale = sqrt(3)*(sdDataset/pi)
+    
+    # Plot the histogram and add the corresponding distribution
+    histTitle = paste('Histogram with Logistic density (alfa=', distLoc,
+                       ', beta=', distScale)
+    
+    hist(dataset, freq=FALSE, main=histTitle, ylim=c(0, 0.3))
+    
+    # Overlap the beta curve to the obtained histogram
+    curve(dlogis(x, location=distLoc, scale=distScale), col="darkblue",
+          lwd=2, add=TRUE)
+  } else if (PLOT_DENSITY == 'Cauchy') {
+    # TODO(jpleitao): Implement this
+  } else if (PLOT_DENSITY == 'Student') {
+    # TODO(jpleitao): Implement this
+  }
+  
+  else {
     # Just plot the histogram
-    hist(dataset, freq=FALSE)
+    hist(dataset)
   }
 }
--- A2/code/main.R
+++ A2/code/main.R
@@ -2,13 +2,26 @@
 # 2016/2017 School Year
 # Doctoral Program in Information Science and Technology - Statistics
 
-# Add source to scripts
-source('/media/jpleitao/Data/PhD/PDCTI/Statistics/Statistics-Assignments/A2/code/DrawHistogram.R')
-source('/media/jpleitao/Data/PhD/PDCTI/Statistics/Statistics-Assignments/A2/code/LoadDataset.R')
+main <- function() {
+  # Add source to scripts
+  source(paste('/media/jpleitao/Data/PhD/PDCTI/Statistics/Statistics-Assignments',
+               '/A2/code/DrawHistogram.R', sep=''))
+  source(paste('/media/jpleitao/Data/PhD/PDCTI/Statistics/Statistics-Assignments',
+               '/A2/code/LoadDataset.R', sep=''))
+  
+  # Load dataset
+  dataset <- LoadDataset(paste('/media/jpleitao/Data/PhD/PDCTI/Statistics/', 
+                               'Statistics-Assignments/A2/data/Amostra.xlsx',
+                               sep=''))
+  
+  # Draw histogram and save it to a file
+  DrawHistogram(dataset)
+  
+  return(c(mean(dataset), sd(dataset)))
+  
+}
 
-# Load dataset
-dataset <- LoadDataset("/media/jpleitao/Data/PhD/PDCTI/Statistics/Statistics-Assignments/A2/data/Amostra.xlsx")
+l<- main()
 
-
-# Draw histogram and save it to a file
-DrawHistogram(dataset)
+meanDataset <- l[1]
+sdDataset <- l[2]
